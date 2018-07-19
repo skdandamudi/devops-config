1e420c486c6fe0c66e10bcaedff771e9b5fc236b
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-19 15:18:24 -0400

    Update install-keycloak.sh

--- keycloak/install-keycloak.sh
+++ keycloak/install-keycloak.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -eu
 
+set -o pipefail
+
+yum -y update
+yum -y install git openssh-client curl unzip bash ttf-dejavu coreutils tini gcc gcc-c++ make 
+yum install -y epel-release wget openssl
+
+yum remove -y java
+
+yum install -y java-1.8.0-openjdk-devel
+
+wget -O /tmp/keycloak.zip https://downloads.jboss.org/keycloak/4.1.0.Final/keycloak-4.1.0.Final.zip
