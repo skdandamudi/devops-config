43f13cb90038b724136d0e13b25446cbbc488b35
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-18 22:49:06 -0400

    Update install-jenkins.sh

--- jenkins/install-jenkins.sh
+++ jenkins/install-jenkins.sh
@@ -95,5 +95,5 @@ sleep 5
 
 
 ##Instal nvm
-#sudo -u jenkins curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+#sudo -u jenkins curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
