80c20799e52a87ef16569c43dadca55cbabe99f1
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-19 12:58:47 -0400

    Update config.groovy

--- jenkins/config.groovy
+++ jenkins/config.groovy
@@ -32,14 +32,10 @@ if(!Jenkins.instance.isQuietingDown()) {
     if(jenkins.getCrumbIssuer() == null) {
         jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
         jenkins.save()
-        println 'CSRF Protection configuration has changed.  Enabled CSRF Protection.'
+         
     }
-    else {
-        println 'Nothing changed.  CSRF Protection already configured.'
-    }
+     
 }
-else {
-    println "Shutdown mode enabled.  Configure CSRF protection SKIPPED."
-}
+ 
 
 
