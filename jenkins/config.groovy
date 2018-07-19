e0b008ef908ff54993590d1952a21aadd12aa4f9
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-19 12:31:54 -0400

    Update config.groovy

--- jenkins/config.groovy
+++ jenkins/config.groovy
@@ -2,7 +2,19 @@
 // imports
 import jenkins.model.Jenkins
 
-// get Jenkins instance
+// Sets executors count
 Jenkins jenkins = Jenkins.getInstance()
 jenkins.setNumExecutors(6)
+instance.setSlaveAgentPort([50000])
 jenkins.save()
+
+//Set the administrator email address
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+jenkinsLocationConfiguration.setAdminAddress("sudheer.dandamudi@navitas-tech.com")
+jenkinsLocationConfiguration.save()
+
+//set Git global config
+def gitConfig = inst.getDescriptor("hudson.plugins.git.GitSCM")
+gitConfig.setGlobalConfigName("sscdbot")
+gitConfig.setGlobalConfigEmail("steadystatecd@gmail.com")
+gitConfig.save()
