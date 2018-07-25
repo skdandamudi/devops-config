6699947eae7019d96624e85b8d70ba853c6a8403
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-24 23:46:39 -0400

    Update install-kong.sh

--- kong/install-kong.sh
+++ kong/install-kong.sh
@@ -1,1 +1,18 @@
 
+#!/bin/bash -eu
+
+set -o pipefail
+
+yum -y update
+yum -y install git openssh-client curl unzip bash ttf-dejavu coreutils tini gcc gcc-c++ make 
+yum install -y epel-release wget openssl
+
+yum remove -y java
+
+yum install -y java-1.8.0-openjdk-devel
+
+
+
+rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm
+
+yum -y install postgresql95  postgresql95-server
