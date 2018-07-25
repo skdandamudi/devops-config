518b8835545225fde81e7db0df4cb0407350d1ea
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-24 23:57:13 -0400

    Update install-kong.sh

--- kong/install-kong.sh
+++ kong/install-kong.sh
@@ -16,3 +16,10 @@ yum install -y java-1.8.0-openjdk-devel
 rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm
 
 yum -y install postgresql95  postgresql95-server
+
+service postgresql95 initdb
+
+service postgresql95 start
+
+
+wget -O kong-community-edition-0.14.0.aws.rpm https://bintray.com/kong/kong-community-edition-aws/download_file?file_path=dists/kong-community-edition-0.14.0.aws.rpm
