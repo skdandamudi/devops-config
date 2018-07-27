6f47d78cecb62154a9968f44c1f1f0d81f9bfc3e
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-27 15:30:20 -0400

    Update install-sonar.sh

--- sonar/install-sonar.sh
+++ sonar/install-sonar.sh
@@ -1,35 +1,41 @@
+
 #!/bin/bash -eu
 
 set -o pipefail
 
 yum -y update
 yum -y install git openssh-client curl unzip bash ttf-dejavu coreutils tini gcc gcc-c++ make 
-yum install -y epel-release wget openssl
+yum install -y epel-release wget openssl epel-release
 
 yum remove -y java
 
 yum install -y java-1.8.0-openjdk-devel
 
+echo '* soft nofile 65000' >> /etc/security/limits.conf
+echo '* hard nofile 65000' >> /etc/security/limits.conf
 
 
-wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
+echo 'Installing postgresql.....'
 
-yum -y install sonar
+rpm -Uvh  https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-ami201503-96-9.6-2.noarch.rpm
 
-chkconfig sonar on
+sleep 5
 
-rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm
+ 
+yum -y install postgresql96-server postgresql96-contrib postgresql96
 
-yum -y install postgresql95  postgresql95-server
+echo 'Initialize  postgresql.....'
 
-
-
-service postgresql95 initdb
-
-service postgresql95 start
+systemctl enable postgresql-9.6.service
 
 
-chkconfig postgresql on 
+service postgresql-9.6 initdb
+
+sleep 3
+
+service postgresql-9.6 start
+
+sleep 5
 
 
 
@@ -37,20 +43,48 @@ chkconfig postgresql on
 
 wget -O /tmp/pg.sql https://raw.githubusercontent.com/navitastech-rfad/devops-config/master/sonar/pg.sql
 
+
+echo 'Create Sonar DB.....'
 sudo -u postgres psql -f /tmp/pg.sql
 
 
+s
 wget -O /tmp/pg_hba.conf https://raw.githubusercontent.com/navitastech-rfad/devops-config/master/sonar/pg_hba.conf
 
-mv /tmp/pg_hba.conf /var/lib/pgsql95/data/pg_hba.conf
+mv /tmp/pg_hba.conf /var/lib/pgsql/9.6/data/pg_hba.conf
 
-chown postgres:postgres /var/lib/pgsql95/data/pg_hba.conf
+chown postgres:postgres /var/lib/pgsql/9.6/data/pg_hba.conf
 
-service postgresql95 stop
-service postgresql95 start
+service postgresql-9.6 stop
+
+sleep 5
+
+service postgresql-9.6 start
 
 
+sleep 3
 
-service sonar start
+echo 'Instaling Sonar .....'
+
+groupadd -g 1000 sonar
+useradd -u 1000 -g 1000 sonar
+
+echo "sonar   ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+wget -O /tmp/sonarqube-7.2.1.zip https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-7.2.1.zip
+
+unzip /tmp/sonarqube-7.2.1.zip -d /opt
+
+mv /opt/sonarqube-7.2.1 /opt/sonarqube
+
+wget -O /opt/sonarqube/conf/sonar.properties  https://raw.githubusercontent.com/navitastech-rfad/devops-config/master/sonar/pg_hba.conf
+
+
+chown -R sonar:sonar /opt/sonarqube
+
+
+su - ec2-sonar -c "/opt/sonarqube/bin/linux-x86-64/sonar.sh start"
+
+
 
 
