49c54429d3db887a9b060c6805d15f2384bfe199
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-27 11:11:16 -0400

    Update install-base.sh

--- base/install-base.sh
+++ base/install-base.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash -eu
+
+set -o pipefail
+
+yum -y update
+yum -y install git openssh-client curl unzip bash ttf-dejavu coreutils tini gcc gcc-c++ make jq
+yum install -y epel-release wget openssl
+
+yum remove -y java
+
+yum install -y java-1.8.0-openjdk-devel
