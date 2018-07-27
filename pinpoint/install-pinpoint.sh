901c5cc3d12e140b9eabdb7acefe35f90fc54b6a
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-27 12:28:40 -0400

    Update install-pinpoint.sh

--- pinpoint/install-pinpoint.sh
+++ pinpoint/install-pinpoint.sh
@@ -10,25 +10,32 @@ yum remove -y java
 
 yum install -y java-1.8.0-openjdk-devel
 
+groupadd -g 1000 tomcat
+useradd -u 1000 -g 1000 tomcat
+
+echo "tomcat   ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+wget -O /tmp/tomcat.zip  https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.29/bin/apache-tomcat-8.5.29.zip 
+
+tar zxpvf /tmp/tomcat.zip -C /opt
+
+chown -R tomcat:tomcat /opt/tomcat
+
+rm -rf /opt/tomcat/webapps/*.war
 
 
-wget -O /tmp/hbase.tar.gz http://archive.apache.org/dist/hbase/1.2.6/hbase-1.2.6-bin.tar.gz
+#wget -O /tmp/hbase.tar.gz http://archive.apache.org/dist/hbase/1.2.6/hbase-1.2.6-bin.tar.gz
 
-tar -xvf /tmp/hbase.tar.gz -C /opt/
+#tar -xvf /tmp/hbase.tar.gz -C /opt/
  
-/opt/habse/start-hbase.sh
+#/opt/habse/start-hbase.sh
 
-yum install tomcat8 
+#sleep 10
 
+#su - tomcat -c  "wget -O /usr/share/tomcat8/webapps/ROOT.war  https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-web-1.7.3.war"
 
-service tomcat8 start
+#sleep 10
 
-sleep 10
+#su - tomcat -c "wget -O /usr/share/tomcat8/webapps/collector.war https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-collector-1.7.3.war"
 
-su - tomcat -c  "wget -O /usr/share/tomcat8/webapps/ROOT.war  https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-web-1.7.3.war"
-
-sleep 10
-
-su - tomcat -c "wget -O /usr/share/tomcat8/webapps/collector.war https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-collector-1.7.3.war"
-
-sleep 10
+#sleep 10
