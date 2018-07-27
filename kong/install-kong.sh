02ce53c82bc5dc25e811c4e9f1ab395ecac8028e
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-27 15:11:04 -0400

    Update install-kong.sh

--- kong/install-kong.sh
+++ kong/install-kong.sh
@@ -17,18 +17,23 @@ echo '* hard nofile 65000' >> /etc/security/limits.conf
 
 echo 'Installing postgresql.....'
 
-rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm
+rpm -Uvh  https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-ami201503-96-9.6-2.noarch.rpm
 
 sleep 5
 
-yum -y install postgresql95  postgresql95-server
+ 
+yum -y install postgresql96-server postgresql96-contrib postgresql96
 
 echo 'Initialize  postgresql.....'
 
-service postgresql95 initdb
+systemctl enable postgresql-9.6.service
+
+
+service postgresql-9.6 initdb
+
 sleep 3
 
-service postgresql95 start
+service postgresql-9.6 start
 
 sleep 5
 
@@ -46,13 +51,15 @@ sudo -u postgres psql -f /tmp/pg.sql
 s
 wget -O /tmp/pg_hba.conf https://raw.githubusercontent.com/navitastech-rfad/devops-config/master/kong/pg_hba.conf
 
-mv /tmp/pg_hba.conf /var/lib/pgsql95/data/pg_hba.conf
-chown postgres:postgres /var/lib/pgsql95/data/pg_hba.conf
+mv /tmp/pg_hba.conf /var/lib/pgsql/9.6/data/pg_hba.conf
 
-service postgresql95 stop
+chown postgres:postgres /var/lib/pgsql/9.6/data/pg_hba.conf
+
+service postgresql-9.6 stop
 
 sleep 5
-service postgresql95 start
+
+service postgresql-9.6 start
 
 
 sleep 3
