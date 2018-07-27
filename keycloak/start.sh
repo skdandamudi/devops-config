f87c1fb9620e218f3ce79a85745d16656958965b
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2018-07-27 13:38:38 -0400

    Update start.sh

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup /opt/keycloak/bin/standalone.sh -b $HOSTIP >> /tmp/keycloak_std.log 2>&1 &
+INSTANCEIP=$(curl -s -m 60 http://169.254.169.254/latest/meta-data/local-ipv4)
+
+nohup /opt/keycloak/bin/standalone.sh -b $INSTANCEIP >> /tmp/keycloak_std.log 2>&1 &
