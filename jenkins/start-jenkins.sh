e28119f5c2b7d9e0b0e4f6a4f58d3d9840a51bcc
Sudheer Dandamudi <skdandamudi@users.noreply.github.com>
2019-10-14 20:07:01 -0400

    Update start-jenkins.sh

--- jenkins/start-jenkins.sh
+++ jenkins/start-jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /usr/lib/jvm/jre-11/bin/java -Djenkins.install.runSetupWizard=false -DJENKINS_HOME=/var/jenkins/data -Xmx1024m -XX:MaxPermSize=2048m -jar /opt/jenkins/jenkins.war --httpPort=8080 >> /var/jenkins/logs/jenkins_std.log 2>&1 &
+nohup /usr/lib/jvm/jre-11/bin/java -Djenkins.install.runSetupWizard=false -DJENKINS_HOME=/var/jenkins/data -Xmx1024m -XX:MaxPermSize=2048m -jar /opt/jenkins/jenkins.war --httpPort=8080 --prefix=/jenkins >> /var/jenkins/logs/jenkins_std.log 2>&1 &
